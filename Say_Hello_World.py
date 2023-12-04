29154e999a17985aa0fa9e9e17458359c589ab2c
dinhpt <phan.dinh1412@gmail.com>
2023-12-04 14:56:20 +0700

    Update exercise 1

--- Say_Hello_World.py
+++ Say_Hello_World.py
@@ -1,10 +1,12 @@
 # Here is a sample line of code that can be executed in Python:
 #
 # print("Hello, World!")
+#
 # You can just as easily store a string as a variable and then print it to stdout:
 #
 # my_string = "Hello, World!"
 # print(my_string)
+#
 # The above code will print Hello, World! on your screen. Try it yourself in the editor below!
 #
 # Input Format
@@ -18,6 +20,7 @@
 # Sample Output 0
 #
 # Hello, World!
+#
 ########################################################################
 
 text = "Hello, World!"
