c647dcaa6ae73e450c00fcbc04fbd6856f5a96ad
dinhpt <phan.dinh1412@gmail.com>
2023-12-04 15:31:18 +0700

    update exercise 3

--- Arithmetic_Operators.py
+++ Arithmetic_Operators.py
@@ -12,6 +12,7 @@
 # 8
 # -2
 # 15
+#
 # Input Format
 #
 # The first line contains the first integer, .
@@ -19,7 +20,8 @@
 #
 # Constraints
 #
-#
+# 1 <= a <= 10^10
+# 1 <= b <= 10^10
 #
 # Output Format
 #
@@ -29,13 +31,29 @@
 #
 # 3
 # 2
+#
 # Sample Output 0
 #
 # 5
 # 1
 # 6
+#
 # Explanation 0
+#
+# 3 + 2 -> 5
+# 3 - 2 -> 1
+# 3 * 2 -> 6
+###########################################################################################
 
+a = int(input())
+b = int(input())
 
+sum = a + b
+minus = a - b
+multi = a * b
+print(sum)
+print(minus)
+print(multi)
 
-
+########################################################################
+# You have earned 10.00 points!
