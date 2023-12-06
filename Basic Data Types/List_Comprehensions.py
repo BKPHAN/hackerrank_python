3e34ff581097043a5bb9dc6a6c0a7278757a1359
dinhpt <phan.dinh1412@gmail.com>
2023-12-06 13:59:49 +0700

    Update folder Basic Data Types: Find_the_Runner-Up_Score.py, List_Comprehensions.py

--- Basic Data Types/List_Comprehensions.py
+++ Basic Data Types/List_Comprehensions.py
@@ -2,18 +2,22 @@
 #
 # Example
 #
+# x = 1
+# y = 1
+# z = 2
+# n = 3
 #
+# All permutations of [i, j, k] are:
 #
+# [[0,0,0], [0,0,1], [0,0,2], [0,1,0], [0,1,1], [0,1,2], [1,0,0], [1,0,1], [1,0,2], [1,1,0], [1,1,1], [1,1,2]]
 #
-# All permutations of  are:
-# .
+# Print an array of the elements that do not sum to n = 3.
 #
-# Print an array of the elements that do not sum to .
-#
+# [[0,0,0], [0,0,1], [0,0,2], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0], [1,1,2]]
 #
 # Input Format
 #
-# Four integers  and , each on a separate line.
+# Four integers x,y,z and n, each on a separate line.
 #
 # Constraints
 #
@@ -25,12 +29,16 @@
 # 1
 # 1
 # 2
+#
 # Sample Output 0
 #
 # [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
+#
 # Explanation 0
 #
-# Each variable  and  will have values of  or . All permutations of lists in the form .
+# Each variable  and  will have values of 0 or 1. All permutations of lists in the form
+# [i,j,k] = [[0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0], [1,1,1]].
+#
 # Remove all arrays that sum to n = 2 to leave only the valid permutations.
 #
 # Sample Input 1
@@ -39,8 +47,30 @@
 # 2
 # 2
 # 2
+#
 # Sample Output 1
 #
 # [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 2], [0, 2, 1], [0, 2, 2], [1, 0, 0], [1, 0, 2], [1, 1, 1],
 # [1, 1, 2], [1, 2, 0], [1, 2, 1], [1, 2, 2], [2, 0, 1], [2, 0, 2], [2, 1, 0], [2, 1, 1], [2, 1, 2],
 # [2, 2, 0], [2, 2, 1], [2, 2, 2]]
+#  [7 8 9]]
+########################################################################################################################
+
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+my_list = []
+
+for i in range(x + 1):
+    for j in range(y + 1):
+        for k in range(z + 1):
+            if i + j + k != n:
+                elt = list(map(int, (i, j, k)))
+                my_list.append(elt)
+
+print(my_list)
+
+########################################################################
+# You have earned 10.00 points!
